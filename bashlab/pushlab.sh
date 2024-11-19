a3b020c33a4cb53c0ad900869811d273e124db03
julroubens2005@gmail.com <julroubens2005@gmail.com>
2024-11-18 19:59:26 -0600

    commit unit103 lab1

---
+++ bashlab/pushlab.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#automate the push
+
+git -a *
+git commit -m "Commit lab"
+git push
---
+++ bashlab/unit103lab.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: Rougens Jules
+# Date of latest revision: 
+# Purpose: Print a string to the terminal
+
+echo " Hello world"
