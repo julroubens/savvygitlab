cc506a01def8bef7a9071dfee02c6b242ca49c0f
julroubens2005@gmail.com <julroubens2005@gmail.com>
2024-11-19 20:10:01 -0600

    Correction for unit103 lab2

--- bashlab/unit103lab2.sh
+++ bashlab/unit103lab2.sh
@@ -1,8 +1,8 @@
-# #!bin/bash
+#!bin/bash
 # Declare a variable with as your name
 # Print to the screen Welcome to the Class Name(variable)
 
-name= "Rougens Jules"
-slogan= "Welcome to the class"
+name="Rougens Jules"
+slogan="Welcome to the class"
 
 echo "$slogan, $name!"
