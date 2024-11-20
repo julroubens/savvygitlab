a7102ca024664057d398a6d49e5b492b339a636e
julroubens2005@gmail.com <julroubens2005@gmail.com>
2024-11-19 19:11:14 -0600

    push unit103 lab2

---
+++ bashlab/unit103lab2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Lets create an until loop that takes a variable and adds 1 till it reachs 10
+# Have the script echo out what are current number is
+
+name= "Rougens Jules"
+slogan= "Welcome to the class"
+
+echo "Enter your name:"
+read user_name
+echo "$slogan, $user_name!"
